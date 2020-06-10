a4b2a3b9434156b3fb0a7ff577252c81467e0e69
TuanTT <tuantt2429x@gmail.com>
2020-06-10 18:33:25 +0700

    init database migration

--- database/migrations/2020_06_10_105629_create_posts_table.php
+++ database/migrations/2020_06_10_105629_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('fullname');
-            $table->string('address');
-            $table->string('phone');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->integer('role');
+            $table->integer('id_user');
+            $table->string('title');
+            $table->string('content');
+            $table->string('description');
             $table->timestamps();
         });
     }
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('posts');
     }
 }
---
+++ database/migrations/2020_06_10_105658_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('vaccine_id');
+            $table->integer('user_id');
+            $table->string('customer_name');
+            $table->string('customer_phone');
+            $table->string('customer_email');
+            $table->integer('quantity');
+            $table->double('total');
+            $table->integer('state');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
