75eff69c57bd6431c97a8d39b83ea22aa759cd80
TuanTT <tuantt2429x@gmail.com>
2020-06-18 21:55:03 +0700

    fix ui login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,15 +53,15 @@
       </div>
   </form>
 
-  <div class="social-auth-links text-center">
+  {{-- <div class="social-auth-links text-center">
       <p>- OR -</p>
       <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook" aria-hidden="true"></i> Sign in using
       Facebook</a>
       <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus" aria-hidden="true"></i> Sign in using
       Google+</a>
-  </div>
+  </div> --}}
   <!-- /.social-auth-links -->
-  <a href="{{asset('')}}register" class="text-center">Register a new membership</a>
+  {{-- <a href="{{asset('')}}register" class="text-center">Register a new membership</a> --}}
 
 </div>
 <!-- /.login-box-body -->
